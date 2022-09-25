a6e76e80177b00180d46d1b263cac240000a972c
Polina <ya.polina.fl@gmail.com>
2022-09-25 15:55:29 +0300

    домашнее задание 2

--- src/hw1/Task4.java
+++ src/hw1/Task4.java
@@ -4,7 +4,8 @@ public class Task4 {
     public static void main(String[] args) {
         byte a = 54;
         byte t = (byte)(a/10);
-        byte u=(byte)(a%(t*10));
+        //byte u=(byte)(a%(t*10));
+        byte u=(byte)(a%10);
         int s = t+u;
         System.out.println("сумма цифр двузначного числа равна "+s);
 
