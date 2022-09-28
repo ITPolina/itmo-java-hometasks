c0091e73ea91aad4af3d5d50af3b0d4ec0b27975
Polina <ya.polina.fl@gmail.com>
2022-09-28 16:42:59 +0300

    добавлена Task5 в дз3

---
+++ src/hw3/Task5.java
@@ -0,0 +1,32 @@
+package hw3;
+import java.util.Scanner;
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите целое число от 2 до 100 включительно:");
+        byte z = in.nextByte();
+        byte a = 2, b = 100;
+        byte c = (byte)((b-a)/2);
+        if (z >=2 && z <= 100) {
+            while (c != z) {
+                System.out.println("Загаданное число равно " + c + "? Если да, введите 1, если нет - 0 ");
+                byte ans1 = in.nextByte();
+                if (ans1 == 0) {
+                    System.out.println("Загаданное число больше " + c + "? Если да, введите 1, если нет - 0 ");
+                    byte ans2 = in.nextByte();
+                    if (ans2 == 1) {
+                        a = (byte)(c + 1);
+                        c = (byte)((b - a)/2 + a);
+                    } else {
+                        b = (byte)(c - 1);
+                        c = (byte)((b - a)/2 + a);
+                    }
+
+                } else break;
+
+            } System.out.println("Ура! Мы отгадали ваше число, это: " + c);
+
+
+        } else System.out.println("Некорректный ввод числа");
+    }
+}
